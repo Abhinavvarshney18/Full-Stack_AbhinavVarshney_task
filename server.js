d2bfbb36b36da57ff8ab4942d6d81db7cfbdf562
Abhinavvarshney18 <151738550+Abhinavvarshney18@users.noreply.github.com>
2026-02-19 14:16:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,34 +1,50 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const path = require('path');
 
+const filePath = path.join(__dirname, 'notes.txt');
 
 const server = http.createServer((req, res) => {
 
-    const logFilePath = path.join(__dirname, 'requests.log');
+    const parsedUrl = url.parse(req.url, true);
+    const route = parsedUrl.pathname;
+    const query = parsedUrl.query;
 
-    const dateTime = new Date().toISOString();
+    if (route === '/add' && req.method === 'GET') {
 
-    const logMessage = `${dateTime} | ${req.method} | ${req.url}\n`;
-
-    fs.appendFile(logFilePath, logMessage, (err) => {
-        if (err) {
-            console.error('Error writing log');
+        if (!query.note) {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('400 Bad Request - Note is missing');
+            return;
         }
-    });
 
-    if (req.url === '/' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Welcome to Home Page');
+        fs.appendFile(filePath, query.note + '\n', (err) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error saving note');
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Note Added Successfully');
+        });
     }
-    else if (req.url === '/about' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('This is About Page');
+
+    else if (route === '/notes' && req.method === 'GET') {
+
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err || data.trim() === '') {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('No Notes Found');
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(data);
+        });
     }
-    else if (req.url === '/contact' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('This is Contact Page');
-    }
+
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('404 Not Found');
@@ -37,6 +53,7 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = 3000;
+
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
